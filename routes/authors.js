60e3a6738c99cfee28335a2b9999a7a41796f6d7
JiahangZhou <55719783+JiahangZhou@users.noreply.github.com>
2021-06-26 14:38:55 -0700

    Update authors.js

--- routes/authors.js
+++ routes/authors.js
@@ -15,7 +15,19 @@ router.get('/new', (req, res) => {
 
 // Create Author Route
 router.post('/',  (req, res) => {
-    res.send(req.body.name)
+    const author = new Author({
+        name: req.body.name
+      })
+    author.save((err, newAuthor) => {
+        if(err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else {
+            res.redirect(`authors`)
+        }
+    })
 })
 
 
