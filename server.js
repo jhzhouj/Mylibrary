db9b39e26d5cebb48651b14e1b201c421b268136
JiahangZhou <55719783+JiahangZhou@users.noreply.github.com>
2021-06-28 21:38:07 -0700

    edit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
-
+const bookRouter = require('./routes/books')
 
 
 app.set('view engine', 'ejs')
@@ -36,5 +36,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
