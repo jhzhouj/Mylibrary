4f5d30f64c232f3c4ce6a579de3439ae6488cc23
JiahangZhou <55719783+JiahangZhou@users.noreply.github.com>
2021-06-28 21:39:43 -0700

    Finished

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-
+  require('dotenv').config()
+}
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 //const bodyParser = require('body-parser')
 
+// for post and delete router
+const methodOverride = require('method-override')
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
-
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
+//app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // for bodyParser
 app.use(express.urlencoded({ extended: true }));
@@ -27,12 +30,11 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
 
 // import MongoDB 
-const mongoose = require('mongoose') 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
